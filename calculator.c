221052d6dbbe4bb6dc346e07ce4ea95ca44a1632
BanaSreeteja <106796023+BanaSreeteja@users.noreply.github.com>
2022-06-06 22:05:20 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#argc(Argument count) is int and stores number of command-line arguments passed by user.
+#argv is array of character pointers
+
+int main(int argc, char *argv[])
+{
+  float num1;
+  float num2;
+  char O;                                           #variable O is operator.
+  float Answer;
+
+  printf("Welcome Simple calculation \n");
+  
+  scanf("%f %c %f", &num1, &O, & num2);
+
+  switch(O)
+    {
+    case '/': Answer = num1/num2;
+      break;
+    case '*': Answer = num1*num2;
+      break;
+    case '+': Answer = num1+num2;
+      break;
+    case '-': Answer = num1-num2;
+      break;
+    case '^': Answer = pow(num1,num2);
+      break;
+    default: printf("Invalid Operator");
+    }
+  printf("%.8g %c %.8g =  %.7g \n\n",num1,O, num2, Answer);
+  
+  return 0;
+}
